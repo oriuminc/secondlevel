5c488271042b82e63a5cf70dc330bc488c1450b0
Patrick Connolly <patrick@myplanetdigital.com>
2012-06-28 01:55:38 -0400

    Added rought deploy script skeleton. Currently expect WORKSPACE env var to work.

--- tmp/scripts/rerun-modules/2ndlevel/commands/deploy/default.sh
+++ tmp/scripts/rerun-modules/2ndlevel/commands/deploy/default.sh
@@ -23,6 +23,53 @@
 # Your implementation goes here.
 # ------------------------------
 
-exit $?
+set -e
+
+# Prepare for running Rerun
+export RERUN_MODULES=`readlink -f $WORKSPACE/profile/tmp/scripts/rerun-modules`
+export PATH=$PATH:`readlink -f $WORKSPACE/profile/tmp/scripts/rerun`
+
+PROJECT=2ndlevel
+
+# Execute site build script
+cd $WORKSPACE/profile
+rerun 2ndlevel:build --build-file $WORKSPACE/profile/build-${PROJECT}.make --destination $WORKSPACE/build
+
+git clone --branch develop $REPO $WORKSPACE/acquia
+cd $WORKSPACE/acquia
+git rm -r --force --quiet docroot
+
+# rsync build/ dir into acquia repo docroot/
+# (excluding files according to patterns in file, accessible to developers.)
+rsync --archive --exclude-from=$WORKSPACE/profile/tmp/scripts/docroot-exclude.txt $WORKSPACE/build/ $WORKSPACE/acquia/docroot/
+git add --force docroot/
+
+# Sanity check to see what was commited.
+git status
+
+# If nothing staged, `git diff --cached` exits with 0
+# and so deploy script exits (no error)
+if [ "$(git diff --quiet --exit-code --cached; echo $?)" -eq 0 ]; then
+  echo 'DEPLOY SCRIPT: No changes staged, and so exiting gracefully...'
+  exit 0
+fi
+
+# Script only gets here if something to commit.
+# VVVVVV
+
+# Get commit message from install profile repo
+COMMIT_MSG=`git --git-dir=$WORKSPACE/profile/.git log --oneline --max-count=1`
+
+git commit --message="Profile repo commit $COMMIT_MSG"
+
+# TODO: Remove hardcoded branch?
+#git push origin develop
+
+#echo "Waiting for push to deploy..." && sleep 5
+# Use drush alias in repo so accessible to developers
+#alias drush='drush --yes --alias-path=$WORKSPACE/profile/tmp/scripts'
+#drush @dev updatedb
+#drush @dev fra
+#drush @dev cc all
 
 # Done
